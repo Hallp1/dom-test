02ac39443186d5d5c87cd63aa60fd06dd84c21cb
Hallp1 <108903518+Hallp1@users.noreply.github.com>
2022-07-19 21:58:54 -0500

    Add files via upload

---
+++ DOM-and-Events-Studio/scripts.js
@@ -0,0 +1,11 @@
+// Write your JavaScript code here.
+// Remember to pay attention to page loading!
+window.addEventListener('load',function(){
+let takeOffBtn =document.getElementById("takeoff");
+takeOffBtn.addEventListener('click', function() {
+let isReadyToLaunch = window.confirm("Confirm that the shuttle is ready for takeoff.");
+if(isReadyToLaunch) {
+ }
+ });
+ 
+});
